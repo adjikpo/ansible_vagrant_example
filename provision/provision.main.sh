3ab834a03646689508b168b6f9539dc123d2cbd0
adjikpo <arthur.djikpo@gmail.com>
2021-03-18 16:48:38 +0100

    update the main provision

--- provision/provision.main.sh
+++ provision/provision.main.sh
@@ -3,25 +3,27 @@
 set -e
 set -u
 
+color='\e[1;36m'
+
 #install packages python3-pip vim git tree curl ansible
-echo "INSTALLATION...START"
-apt-get update
-sudo apt -qq install -y python3-pip vim git tree curl 
-echo "install python3-pip, vim, git, tree, curl : OK "
-pip3 install ansible
-echo "install ansible : OK"
+echo "${color} INSTALLATION...START"
 apt-get -qq update
-echo "INSTALLATION...DONE"
+sudo apt -qq install -y python3-pip vim git tree curl 
+echo "${color} install python3-pip, vim, git, tree, curl : OK "
+pip3 install ansible
+echo "${color} install ansible : OK"
+apt-get -qq update
+echo "${color} INSTALLATION...DONE"
 
 #ansible files
-echo "ANSIBLE...STRUCTURE....START"
+echo "${color} ANSIBLE...STRUCTURE....START"
 mkdir -p /home/vagrant/project-ansible
 touch /home/vagrant/project-ansible/hosts
 touch /home/vagrant/project-ansible/playbook.yml
 mkdir -p /home/vagrant/project-ansible/roles
 cd /home/vagrant/project-ansible/roles
 ansible-galaxy init roleProject
-echo "ANSIBLE...STRUCTURE....DONE"
+echo "${color} ANSIBLE...STRUCTURE....DONE"
 tree
 
 #add the clients ip 
